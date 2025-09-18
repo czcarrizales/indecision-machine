3fc6fbab29cbb6b2edb4e8f50eda4c03f2d40172
Chazz Carrizales <czcarrizales@yahoo.com>
2025-09-18 16:19:21 -0700

    testing addChoice function

--- indecision-machine/src/App.jsx
+++ indecision-machine/src/App.jsx
@@ -2,10 +2,10 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [choices, setChoices] = useState([])
+  const [choices, setChoices] = useState(['tester'])
 
   function addChoice() {
-
+    setChoices([...choices, 'test'])
   }
 
   return (
@@ -14,12 +14,13 @@ function App() {
       <button>Decide</button>
       <div>
         <p>Choices</p>
+        {choices}
       </div>
       <div>
         <p>Option</p>
         <input type="text" />
       </div>
-      <button>Add</button>
+      <button onClick={() => addChoice()}>Add</button>
       <button>Remove</button>
     </div>
     </>
