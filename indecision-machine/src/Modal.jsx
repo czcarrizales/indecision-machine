f7ad30dc26c371c958b13645f465fa37d430a836
Chazz Carrizales <czcarrizales@yahoo.com>
2025-09-18 21:04:58 -0700

    check if modal is open or if option selected before delete

--- indecision-machine/src/Modal.jsx
+++ indecision-machine/src/Modal.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import './Modal.css'
 
-function Modal({ choices, selectedChoice, closeModal }) {
+function Modal({ option, choices, selectedChoice, closeModal }) {
     return (
         <div className='modal'>
-                <p>You should: {choices.map(choice => {
-                    if (choice.id === selectedChoice) {
-                        return choice.option
-                    }
-                })}</p>
-                <button className='modal-button' onClick={() => closeModal(false)}>Okay</button>
+            {
+                selectedChoice == ''
+                    ?
+                    <p>You must select an option to delete!</p>
+                    :
+                    <p>You should: {choices.map(choice => {
+                        if (choice.id === selectedChoice) {
+                            return choice.option
+                        }
+                    })}</p>
+            }
+            <button className='modal-button' onClick={() => closeModal(false)}>Okay</button>
         </div>
     )
 }
