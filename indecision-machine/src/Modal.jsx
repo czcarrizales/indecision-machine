2f5a5e3a55a09644aeaf8ebd119cc52b698c437a
Chazz Carrizales <czcarrizales@yahoo.com>
2025-09-18 20:47:37 -0700

    styling modal

--- indecision-machine/src/Modal.jsx
+++ indecision-machine/src/Modal.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import './Modal.css'
 
-function Modal({choices, selectedChoice, closeModal}) {
-  return (
-    <div className='modal'>
-        <p>You should: {choices.map(choice => {
-            if (choice.id === selectedChoice) {
-                return choice.option
-            }
-        })}</p>
-        <button className='modal-button' onClick={() => closeModal(false)}>Okay</button>
-    </div>
-  )
+function Modal({ choices, selectedChoice, closeModal }) {
+    return (
+        <div className='modal'>
+                <p>You should: {choices.map(choice => {
+                    if (choice.id === selectedChoice) {
+                        return choice.option
+                    }
+                })}</p>
+                <button className='modal-button' onClick={() => closeModal(false)}>Okay</button>
+        </div>
+    )
 }
 
 export default Modal
